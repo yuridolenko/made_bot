8168a080bfe05996e80dd8b9e8e46bf861429402
ydolenko <yurii.dolenko@tain.com>
2021-03-31 09:01:20 +0300

    echo bot created

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import logging
+
+# Logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# TODO: refactor, create class Bot, use it for different games
+# TODO: move token to config
+# t.me/made_games_bot.
+# token : 1794408127:AAFn19tXFkCuhn7eaPaa2xqAm7woqai7GF0
+
+updater = Updater(token='1794408127:AAFn19tXFkCuhn7eaPaa2xqAm7woqai7GF0', use_context=True);
 
 
-# Press the green button in the gutter to run the script.
+def start(update: Update, _: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        f'Здарова, {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def echo(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    updater = Updater("1794408127:AAFn19tXFkCuhn7eaPaa2xqAm7woqai7GF0")
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
